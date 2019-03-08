d951d833be5ce9668da6923bf088433f4129299a
Jin <jame.bond0523@gmail.com>
2019-03-09 03:51:39 +0800

    updated version

--- portfolio/resources/views/index.blade.php
+++ portfolio/resources/views/index.blade.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Emīls Gulbis | Front-end developer</title>
-    <meta name="description" content="Love to craft beautiful designs into live user interfaces with the best user experience in mind">
-    <link rel="icon" href="/favicon.png" sizes="64x64">
-    <link rel="stylesheet" href="/css/app.css">
-    <meta name="theme-color" content="#4c33fb">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Emīls Gulbis | Front-end developer</title>
+<meta name="description" content="Love to craft beautiful designs into live user interfaces with the best user experience in mind">
+<link rel="icon" href="/favicon.png" sizes="64x64">
+<link rel="stylesheet" href="/css/app.css">
+<meta name="theme-color" content="#4c33fb">
 </head>
 
 <body>
